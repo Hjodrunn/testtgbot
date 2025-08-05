04a87e00ef0699ffe9abb0e54740e0226322fa6f
Hjodrunn <zlaggeroga@gmail.com>
2025-08-05 22:59:22 +0300

    11

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import { useEffect } from 'react';
 import './App.css';
-import {useTelegram} from './src/hooks/useTelegram.js'
+import {useTelegram} from './src/components/hooks/useTelegram.js'
 
 
 const tg = window.Telegram.WebApp;
